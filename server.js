2d64d856300ea1fcc202e194c71e59cd4a0457f2
Arthur CRAHÉ <crahe.arthur@gmail.com>
2023-01-22 21:58:48 +0100

    comm with teacher socket

--- server.js
+++ server.js
@@ -3,57 +3,68 @@ const express = require('express'),
        http = require('http'),
        server = http.createServer(app),
        { Server } = require("socket.io"),
-       io = new Server(server, { allowEIO3: true });
+       io = new Server(server);
+
+let quizzTitle = null;
+let groupName = null;
+let teacher = null;
+let sessionStatus = "notConnected";
+let numberOfConnectedStudents = 0;
 
 // Enable access to the src folder :
 app.use(express.static('src')); // https://stackoverflow.com/a/54747432/19601188
 
-let numberOfStudentConnected = 0,
-       numberOfRegisteredStudents = 0,
-       sessionStatus = "not created"; // created -- started
-
 // Express routing :
 app.get('/', (dataFromClient, serverResponse) => {
-       console.log('index.html requested');
-       serverResponse.sendFile(__dirname + '/index.html');
-});
-app.get('/student', (dataFromClient, serverResponse) => {
-       serverResponse.sendFile(__dirname + '/student.html');
-});
-app.get('/student.html', (dataFromClient, serverResponse) => {
        serverResponse.sendFile(__dirname + '/student.html');
 });
 
 io.on('connection', function (client) {
-       console.log('a user connected');
-       client.to('teacher').emit('student connected changed', ++numberOfStudentConnected);
-
-       // Signals from students (client side) when they are registered :
-       client.on('student registered', (studentName) => {
-              if (client.username == undefined) {
-                     numberOfRegisteredStudents++;
-              }
-              client.username = studentName;
-              client.to('teacher').emit('students registered changed', {
-                     id: client.modifiedID,
-                     studentName: client.username,
-                     status: "registered",
-                     numberOfRegisteredStudents: numberOfRegisteredStudents
-              });
-       });
-
-       client.on('disconnecting', () => {
-              client.to('teacher').emit('student connected changed', --numberOfStudentConnected);
-              if (client.modifiedID != undefined && client.username != undefined) {
-                     client.to('teacher').emit('students registered changed', {
-                            id: client.modifiedID,
-                            studentName: client.username,
-                            status: "not registered anymore",
-                            numberOfRegisteredStudents: --numberOfRegisteredStudents
+       if (client.handshake.headers.origin == 'http://e-quizz.test') {
+              client.on('resetSession', () => {
+                     quizzTitle = null;
+                     groupName = null;
+                     teacher = null;
+                     sessionStatus = "notConnected";
+                     numberOfConnectedStudents = 0;
+                     io.to("teacher").emit('sessionStatus', {
+                            sessionStatus: sessionStatus,
+                            quizzTitle: quizzTitle,
+                            groupName: groupName,
+                            teacher: teacher,
+                            numberOfConnectedStudents: numberOfConnectedStudents
                      });
-              }
-       });
+                     return;
+              })
+              client.join("teacher");
+
+              client.on('createSession', (data) => {
+                     io.to('teacher').emit('sessionCreated', data);
+                     quizzTitle = data.quizzName;
+                     teacher = data.mail;
+                     groupName = data.groupName;
+                     sessionStatus = "SessionStatus";
+                     io.to("teacher").emit('sessionStatus', {
+                            sessionStatus: sessionStatus,
+                            quizzTitle: quizzTitle,
+                            groupName: groupName,
+                            teacher: teacher,
+                            numberOfConnectedStudents: numberOfConnectedStudents
+                     });
+              });
+
+              client.on('startSession', (data) => {
+                     io.to('teacher').emit('sessionStarted', data);
+                     //sessionStatus = "created";
+              });
+              io.to("teacher").emit('sessionStatus', {
+                     sessionStatus: sessionStatus,
+                     quizzTitle: quizzTitle,
+                     groupName: groupName,
+                     teacher: teacher,
+                     numberOfConnectedStudents: numberOfConnectedStudents
+              });
+       }
 });
 
-server.listen(8100, () => {
-});
+server.listen(8100);
