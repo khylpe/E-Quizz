a6a921852159061d5e22ec3e227462f072908fc5
Arthur CRAHÉ <crahe.arthur@gmail.com>
2023-05-23 21:06:07 +0200

    Renamed Class

--- src/scripts/classes/front/FrontSession.js
+++ src/scripts/classes/front/FrontSession.js
@@ -1,121 +1,121 @@
-import FrontGlobal from "./FrontGlobal.js";
+       import FrontGlobal from "./FrontGlobal.js";
 
-export default class Teacher extends FrontGlobal {
-       constructor() {
-              super();
-       }
-       
-       setSessionStatus(sessionInformation) {
-              if (this.getCurrentSection().id == "sectionCreateSession") {
-                     document.querySelector('#sessionInfo').style.display = "none";
-                     return;
+       export default class FrontSession extends FrontGlobal {
+              constructor() {
+                     super();
               }
-              document.querySelector('#sessionInfo').style.display = "block";
-              let status;
-              if (sessionInformation.sessionStatus == "CreateSession")
-                     status = "Pas connecté";
-              else if (sessionInformation.sessionStatus == "SessionStatus")
-                     status = "Session créée, en attente de démarrage";
-              else if (sessionInformation.sessionStatus == "DisplayQuestions")
-                     status = "Session démarrée";
-              else if (sessionInformation.sessionStatus == "SessionEnded")
-                     status = "Session terminée";
-              else if (sessionInformation.sessionStatus == "SessionClosed")
-                     status = "Session fermée";
-              else
-                     status = "Session inconnue";
+              
+              setSessionStatus(sessionInformation) {
+                     if (this.getCurrentSection().id == "sectionCreateSession") {
+                            document.querySelector('#sessionInfo').style.display = "none";
+                            return;
+                     }
+                     document.querySelector('#sessionInfo').style.display = "block";
+                     let status;
+                     if (sessionInformation.sessionStatus == "CreateSession")
+                            status = "Pas connecté";
+                     else if (sessionInformation.sessionStatus == "SessionStatus")
+                            status = "Session créée, en attente de démarrage";
+                     else if (sessionInformation.sessionStatus == "DisplayQuestions")
+                            status = "Session démarrée";
+                     else if (sessionInformation.sessionStatus == "SessionEnded")
+                            status = "Session terminée";
+                     else if (sessionInformation.sessionStatus == "SessionClosed")
+                            status = "Session fermée";
+                     else
+                            status = "Session inconnue";
 
-              document.querySelector('#sessionStatusInfo').innerHTML = `Status de la session : ${status}`;
-              document.querySelector('#teacherInfo').innerHTML = `Enseignant : ${sessionInformation.teacher}`;
-              document.querySelector('#quizzInfo').innerHTML = `Quizz : ${sessionInformation.quizzTitle}`;
-              document.querySelector('#groupInfo').innerHTML = `Groupe : ${sessionInformation.groupName}`;
-       }
+                     document.querySelector('#sessionStatusInfo').innerHTML = `Status de la session : ${status}`;
+                     document.querySelector('#teacherInfo').innerHTML = `Enseignant : ${sessionInformation.teacher}`;
+                     document.querySelector('#quizzInfo').innerHTML = `Quizz : ${sessionInformation.quizzTitle}`;
+                     document.querySelector('#groupInfo').innerHTML = `Groupe : ${sessionInformation.groupName}`;
+              }
 
-       setStudentList(studentName, status, numberOfRegisteredStudents) {
-              document.querySelector('#studentListTitle').innerHTML = `Liste des étudiants prêts et enregistrés (${numberOfRegisteredStudents})`;
+              setStudentList(studentName, status, numberOfRegisteredStudents) {
+                     document.querySelector('#studentListTitle').innerHTML = `Liste des étudiants prêts et enregistrés (${numberOfRegisteredStudents})`;
 
-              if (status == "not registered anymore") {
-                     if (!document.querySelector(`[value="${studentName}"]`)) {
+                     if (status == "not registered anymore") {
+                            if (!document.querySelector(`[value="${studentName}"]`)) {
+                                   let ul = document.getElementById("studentList");
+                                   let li = document.createElement("li");
+                                   li.setAttribute("value", `${studentName}`);
+
+                                   li.classList = "list-group-item list-group-item-action list-group-item-warning d-flex justify-content-center align-items-start";
+                                   let div = document.createElement("div");
+                                   div.classList = "ms-2";
+                                   div.setAttribute("id", "studentName");
+                                   div.appendChild(document.createTextNode(studentName));
+                                   li.appendChild(div);
+                                   ul.appendChild(li);
+                            } else {
+                                   document.querySelector(`[value="${studentName}"]`).classList = "list-group-item list-group-item-action list-group-item-warning d-flex justify-content-center align-items-start";
+                            }
+                     }
+                     else if (!document.querySelector(`[value="${studentName}"]`)) {
                             let ul = document.getElementById("studentList");
                             let li = document.createElement("li");
                             li.setAttribute("value", `${studentName}`);
 
-                            li.classList = "list-group-item list-group-item-action list-group-item-warning d-flex justify-content-center align-items-start";
+                            if (status == "registered") {
+                                   li.classList = "list-group-item list-group-item-action list-group-item-success d-flex justify-content-center align-items-start";
+
+                            } else if (status == "not registered") {
+                                   li.classList = "list-group-item list-group-item-action list-group-item-danger d-flex justify-content-center align-items-start";
+                            } else {
+                                   li.classList = "list-group-item list-group-item-action list-group-item-warning d-flex justify-content-center align-items-start";
+                            }
                             let div = document.createElement("div");
                             div.classList = "ms-2";
                             div.setAttribute("id", "studentName");
                             div.appendChild(document.createTextNode(studentName));
                             li.appendChild(div);
                             ul.appendChild(li);
-                     } else {
-                            document.querySelector(`[value="${studentName}"]`).classList = "list-group-item list-group-item-action list-group-item-warning d-flex justify-content-center align-items-start";
+                     }
+                     else if (status == "registered") {
+                            document.querySelector(`[value="${studentName}"]`).classList = "list-group-item list-group-item-action list-group-item-success d-flex justify-content-center align-items-start";
                      }
               }
-              else if (!document.querySelector(`[value="${studentName}"]`)) {
-                     let ul = document.getElementById("studentList");
-                     let li = document.createElement("li");
-                     li.setAttribute("value", `${studentName}`);
 
-                     if (status == "registered") {
-                            li.classList = "list-group-item list-group-item-action list-group-item-success d-flex justify-content-center align-items-start";
-
-                     } else if (status == "not registered") {
-                            li.classList = "list-group-item list-group-item-action list-group-item-danger d-flex justify-content-center align-items-start";
-                     } else {
-                            li.classList = "list-group-item list-group-item-action list-group-item-warning d-flex justify-content-center align-items-start";
-                     }
-                     let div = document.createElement("div");
-                     div.classList = "ms-2";
-                     div.setAttribute("id", "studentName");
-                     div.appendChild(document.createTextNode(studentName));
-                     li.appendChild(div);
-                     ul.appendChild(li);
+              setNumberOfConnectedStudents(numberOfConnectedStudents, selector) {
+                     document.querySelector(selector).innerHTML = numberOfConnectedStudents;
               }
-              else if (status == "registered") {
-                     document.querySelector(`[value="${studentName}"]`).classList = "list-group-item list-group-item-action list-group-item-success d-flex justify-content-center align-items-start";
-              }
-       }
 
-       setNumberOfConnectedStudents(numberOfConnectedStudents, selector) {
-              document.querySelector(selector).innerHTML = numberOfConnectedStudents;
-       }
-
-       setQuestion(question, answers, questionNumber, numberOfQuestions, selectorForQuestion, selectorForAnswers) {
+              setQuestion(question, answers, questionNumber, numberOfQuestions, selectorForQuestion, selectorForAnswers) {
+                     
+                     answers.forEach((answer, index) => {
+                            if (answer == "" || answer == " " || answer == null || answer == undefined) {
+                                   answers.splice(index, 1);
+                            }
+                     });
               
-              answers.forEach((answer, index) => {
-                     if (answer == "" || answer == " " || answer == null || answer == undefined) {
-                            answers.splice(index, 1);
+                     document.querySelector(selectorForAnswers).innerHTML = "";
+
+                     if (!question.includes("?") && !question.includes(" ?")) {
+                            document.querySelector(selectorForQuestion).innerHTML = question + ` ? (${questionNumber}/${numberOfQuestions})`;
+                     } else {
+                            document.querySelector(selectorForQuestion).innerHTML = question + ` (${questionNumber}/${numberOfQuestions})`;
                      }
-              });
-       
-              document.querySelector(selectorForAnswers).innerHTML = "";
 
-              if (!question.includes("?") && !question.includes(" ?")) {
-                     document.querySelector(selectorForQuestion).innerHTML = question + ` ? (${questionNumber}/${numberOfQuestions})`;
-              } else {
-                     document.querySelector(selectorForQuestion).innerHTML = question + ` (${questionNumber}/${numberOfQuestions})`;
+
+                     let divContainingAnswers = document.createElement('div');
+                     divContainingAnswers.classList = "d-flex flex-column justify-content-center align-items-start";
+                     document.querySelector(selectorForAnswers).appendChild(divContainingAnswers);
+
+                     answers.forEach((answer, index) => {
+                            let answerP = document.createElement('p');
+                            answerP.classList = 'list-group-item border rounded-end rounded-start border-primary-subtle';
+
+                            let answerLetter = document.createElement('span');
+                            answerLetter.classList = `fs-1 text-info`;
+                            answerLetter.innerText = `${String.fromCharCode(65 + index)}. `;
+                            answerP.appendChild(answerLetter);
+
+                            let answerSpan = document.createElement('span');
+                            answerSpan.classList = `fs-1`;
+                            answerSpan.innerText = `${answer}`;
+                            answerP.appendChild(answerSpan);
+
+                            divContainingAnswers.appendChild(answerP);
+                     });              
               }
-
-
-              let divContainingAnswers = document.createElement('div');
-              divContainingAnswers.classList = "d-flex flex-column justify-content-center align-items-start";
-              document.querySelector(selectorForAnswers).appendChild(divContainingAnswers);
-
-              answers.forEach((answer, index) => {
-                     let answerP = document.createElement('p');
-                     answerP.classList = 'list-group-item border rounded-end rounded-start border-primary-subtle';
-
-                     let answerLetter = document.createElement('span');
-                     answerLetter.classList = `fs-1 text-info`;
-                     answerLetter.innerText = `${String.fromCharCode(65 + index)}. `;
-                     answerP.appendChild(answerLetter);
-
-                     let answerSpan = document.createElement('span');
-                     answerSpan.classList = `fs-1`;
-                     answerSpan.innerText = `${answer}`;
-                     answerP.appendChild(answerSpan);
-
-                     divContainingAnswers.appendChild(answerP);
-              });              
        }
-}
