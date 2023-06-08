8f3718928d965401fad18e2368ad16ea99664e5f
ROOS Tristan <tristan.roos.tr@gmail.com>
2023-06-08 16:29:52 +0200

    08/06/2023

--- receptionEnvoiTrame.js
+++ receptionEnvoiTrame.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 import { SerialPort } from "serialport";
 import { ReadlineParser } from '@serialport/parser-readline'
 const port = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 9600 });
+let studentsAnswers = [];
 
 const socket = io("http://192.168.0.254:8100", {
 	transports:
@@ -13,21 +14,17 @@ const socket = io("http://192.168.0.254:8100", {
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '!' }));
 
-let studentsAnswers = [];
+parser.on('data', (data) => {
+    console.log('Data:', data);
+	let traduit = hex2a(data);
+    let startOfTrame = traduit.indexOf("$");
+    let onlyData = traduit.slice(startOfTrame+1, traduit.length);
+    console.log("trame traduite propre : ", onlyData);
+	studentsAnswers.push(traitertrame(onlyData));
+});
 
 socket.on("connect", () => {
-	// Quand on recoie des données : 
 	console.log("connected");
-});
-
-socket.on("disconnect", () => {
-	// Quand on recoie des données : 
-	console.log("disconnected");
-});
-
-socket.on('getStudentsAnswers', (callback) => {
-	console.log("students answers before sending it (should be the same than after): ");
-	callback(studentsAnswers);
 });
 
 socket.on("questionNumber", (questionNumber) => {
@@ -35,27 +32,25 @@ socket.on("questionNumber", (questionNumber) => {
 	console.log("Question number is : ", questionNumber);
 });
 
-parser.on('data', (data) => {
-       console.log('Data:', data);
-	let trameComplete = "";
+socket.on('getStudentsAnswers', (callback) => {
+	callback(studentsAnswers);
+});
 
-	let traduit = hex2a(data);
-       let startOfTrame = traduit.indexOf("$");
-       traduit = traduit.slice(startOfTrame+1, traduit.length);
-       console.log("trame traduite propre : ", traduit);
-       traitertrame(traduit);
+socket.on("disconnect", () => {
+	// Quand on recoie des données : 
+	console.log("disconnected");
 });
 
 /////////////////////////////////////////////////////////////////
-function traitertrame(trame) {
 
+function traitertrame(trame) {
 	let numBoitier = trame[0] + trame[1];
 	let numQuestion = trame[2] + trame[3];
 
 	let choixDeReponse = trame[4];
-
-	for (let i = 5 ; i < trame.length ; i++) {
-		choixDeReponse += trame[i];
+	for (let i = 5 ; trame[i] != "V" ; i++) {
+		if(trame[i] != "V")
+			choixDeReponse += trame[i];
 	}
 
 	console.log("Trame complète", trame);
@@ -75,4 +70,3 @@ function traitertrame(trame) {
 function hex2a(hexx) {
 	return hexx.toString();
 };
-//////////////////////////////////////////////////////////////////////////////
